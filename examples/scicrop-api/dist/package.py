4153ec0c2b456c2c7c4623260662140650e418c0
Samara Marques <103200488+samrqs@users.noreply.github.com>
2025-05-16 15:54:21 -0300

    Update package.py

--- examples/scicrop-api/dist/package.py
+++ examples/scicrop-api/dist/package.py
@@ -1,25 +1,50 @@
 import sys
+import argparse
+import json
+from pathlib import Path
 from infinitestack import scicrop_api
+from infinitestack import collect
 
 
-def process_data(args):
-    lat, long = args[0], args[1]
-    print("LIVE ", scicrop_api.get_weather_data(lat, long))
-    print("FORECAST ", scicrop_api.get_weather_forecast(lat, long))
+def process_data(lat, lon):
+    print("LIVE", scicrop_api.get_weather_data(lat, lon))
+    # print("FORECAST", scicrop_api.get_weather_forecast(lat, lon))
 
 
-def main():
-    args = sys.argv[1:]
-    if not args:
-        print("Usage: python package.py arg1 arg2 ...")
-        sys.exit(1)
+def main(workflow_id, project_name):
 
-    print(f"Received {len(args)} arguments:")
-    for i, arg in enumerate(args, start=1):
-        print(f"Argument {i}: {arg}")
+    bridge = collect.Bridge(project_name)
+    project_id = bridge.project_id
 
-    process_data(args)
+    filePath = Path(f'/tmp/is/{workflow_id}.json')
+
+    if not filePath.exists():
+        print('File not found')
+        exit(1)
+    try:
+        with open(filePath, 'r') as f:
+
+            data = json.load(f)
+            lat = float(data["lat"])
+            lon = float(data["lon"])
+    except (json.JSONDecodeError, KeyError, ValueError) as e:
+        print(f"Invalid input format: {e}")
+        print("Expected format: '{\"lat\": value, \"lon\": value}'")
+        exit(1)
+
+    print(f"Parsed lat: {lat}, lon: {lon}")
+    process_data(lat, lon)
 
 
 if __name__ == "__main__":
-    main()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--workflow_id', type=str, required=True)
+    parser.add_argument('--project_name', type=str, required=True)
+    args, _ = parser.parse_known_args()
+
+    workflow_id = args.workflow_id
+    project_name = args.project_name
+
+
+    main(workflow_id, project_name)
