a037fdb3612701d56a7a7c355ec1d28351d4df66
Joao Fagundes <joao.lima@scicrop.com>
2025-05-20 14:53:07 -0300

    feat: changing endpoint logic

--- examples/scicrop-api/dist/package.py
+++ examples/scicrop-api/dist/package.py
@@ -1,14 +1,14 @@
 import argparse
 import json
 from pathlib import Path
-from infinitestack import scicrop_api
+from infinitestack.scicrop_api import SciCropClient
 from infinitestack import orm
-from infinitestack import collect
 from infinitestack import package_wrapper
 
 
-def process_response_data(lat, lon):
-    return scicrop_api.get_weather_data(lat, lon)
+def process_response_data(data):
+    client = SciCropClient()
+    return client.call_endpoint("weather-current", data)
 
 
 def process_request_data(workflow_id):
@@ -20,29 +20,26 @@ def process_request_data(workflow_id):
     try:
         with open(file_path, 'r') as f:
             data = json.load(f)
-            lat = float(data["lat"])
-            lon = float(data["lon"])
     except (json.JSONDecodeError, KeyError, ValueError) as e:
         print(f"Invalid input format: {e}")
         print("Expected format: '{\"lat\": value, \"lon\": value}'")
         exit(1)
 
-    return lat, lon
+    return data
 
 
-def build_request(lat, lon):
-
-    json_response = process_response_data(lat, lon)
+def build_request(data):
+    json_response = process_response_data(data)
     request = package_wrapper.build_request("scicrop-api", json_response)
     print(f"request {request}")
     return request
 
 
-def save_database(lat, lon, database_names, project_name):
+def save_database(data, database_names, project_name):
     for database in database_names:
         my_orm = orm.Orm(database)
         my_orm.set_project(project_name)
-        my_orm.insert_data_json(build_request(lat, lon))
+        my_orm.insert_data_json(build_request(data))
 
 
 def get_database_names_from_package():
@@ -51,8 +48,8 @@ def get_database_names_from_package():
 
 def main(workflow_id, project_name):
     database_names = get_database_names_from_package()
-    lat, lon = process_request_data(workflow_id)
-    save_database(lat, lon, database_names, project_name)
+    data = process_request_data(workflow_id)
+    save_database(data, database_names, project_name)
 
 
 if __name__ == "__main__":
